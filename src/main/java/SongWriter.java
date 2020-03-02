909c7a2c2788de30454bc1407341f0486d7c97f8
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 23:30:59 +0100

    Multiple lines methods

--- src/main/java/SongWriter.java
+++ src/main/java/SongWriter.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class SongWriter {
     String[] text;
 
     public SongWriter(String[] text){this.text=text;};
     public String getSingleLine(int index){return text[index-1];}
-    public String[] getSomeLines(int indexStart,int indexEnd){return null;}
+    public String[] getSomeLines(int indexStart,int indexEnd){return Arrays.copyOfRange(text,indexStart-1,indexEnd);}
     public String[] getLines(){return null;}
 
 }
