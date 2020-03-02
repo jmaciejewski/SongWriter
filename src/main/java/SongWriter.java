2d43b841f0965bcc8a8a6d067e102abe33019911
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 23:45:22 +0100

    Final exception commit

--- src/main/java/SongWriter.java
+++ src/main/java/SongWriter.java
@@ -9,6 +9,7 @@ public class SongWriter {
         return text[index-1];}
     public String[] getSomeLines(int indexStart,int indexEnd)throws IllegalArgumentException{
         if(indexStart<1||indexEnd>text.length) throw new IllegalArgumentException();
+        if(indexStart>indexEnd) throw new IllegalArgumentException();
         return Arrays.copyOfRange(text,indexStart-1,indexEnd);}
     public String[] getLines(){return text;}
 
