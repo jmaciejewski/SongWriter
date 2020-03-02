acc43f34d71f728a0f9c7830a2cfb17638bb5a82
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 23:42:04 +0100

    Single lines method exceptions

--- src/main/java/SongWriter.java
+++ src/main/java/SongWriter.java
@@ -4,8 +4,12 @@ public class SongWriter {
     String[] text;
 
     public SongWriter(String[] text){this.text=text;};
-    public String getSingleLine(int index){return text[index-1];}
-    public String[] getSomeLines(int indexStart,int indexEnd){return Arrays.copyOfRange(text,indexStart-1,indexEnd);}
+    public String getSingleLine(int index)throws IllegalArgumentException{
+        if(index<1||index>text.length) throw new IllegalArgumentException();
+        return text[index-1];}
+    public String[] getSomeLines(int indexStart,int indexEnd){
+
+        return Arrays.copyOfRange(text,indexStart-1,indexEnd);}
     public String[] getLines(){return text;}
 
 }
