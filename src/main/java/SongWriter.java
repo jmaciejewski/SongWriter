ee01ddb862e0f150d810177d0f9afe72af37b7a9
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 23:44:08 +0100

    many lines methods exceptions

--- src/main/java/SongWriter.java
+++ src/main/java/SongWriter.java
@@ -7,8 +7,8 @@ public class SongWriter {
     public String getSingleLine(int index)throws IllegalArgumentException{
         if(index<1||index>text.length) throw new IllegalArgumentException();
         return text[index-1];}
-    public String[] getSomeLines(int indexStart,int indexEnd){
-
+    public String[] getSomeLines(int indexStart,int indexEnd)throws IllegalArgumentException{
+        if(indexStart<1||indexEnd>text.length) throw new IllegalArgumentException();
         return Arrays.copyOfRange(text,indexStart-1,indexEnd);}
     public String[] getLines(){return text;}
 
