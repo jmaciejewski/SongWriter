093fbda0e230960f9afca129d3d915de7bb059e7
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 23:17:49 +0100

    Single line method

--- src/main/java/SongWriter.java
+++ src/main/java/SongWriter.java
@@ -2,7 +2,7 @@ public class SongWriter {
     String[] text;
 
     public SongWriter(String[] text){this.text=text;};
-    public String getSingleLine(int index){return null;}
+    public String getSingleLine(int index){return text[index-1];}
     public String[] getSomeLines(int indexStart,int indexEnd){return null;}
     public String[] getLines(){return null;}
 
