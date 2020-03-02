b4097ee9b91ffb5d70e1084b073a7e924dcd5cc8
jmaciejewski <32633800+jmaciejewski@users.noreply.github.com>
2020-03-02 23:23:34 +0100

    Multiple lines tests

--- src/test/java/SongWriterTest.java
+++ src/test/java/SongWriterTest.java
@@ -17,6 +17,15 @@ public class SongWriterTest {
             "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
             "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
     };
+    String[] fragment2to7 ={"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+            "On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    };
+    String[] fifthLine ={"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+    };
     SongWriter songWriter;
 
     @Before
@@ -36,6 +45,13 @@ public class SongWriterTest {
     public void test_last_line(){
         assertEquals(text[11],songWriter.getSingleLine(12));
     }
-
+    @Test
+    public void test_some_lines(){
+        assertEquals(fragment2to7,songWriter.getSomeLines(2,7));
+    }
+    @Test
+    public void test_some_lines_one(){
+        assertEquals(fifthLine,songWriter.getSomeLines(5,5));
+    }
 
 }
